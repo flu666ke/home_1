f2d77243d11053061f84d1b55a7a0f2321be07e3
flu666ke <godivaden53@gmail.com>
2019-03-24 17:21:50 +0200

    home_1

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ var images = document.querySelectorAll('#eye img');
 
 var i = 0;
 
+var slideLines = document.querySelectorAll('.slider #slide');
+
+var j = 0;
+
 btnSlide.onclick = function () {
 
     images[i].style.display = 'none';
@@ -18,4 +22,17 @@ btnSlide.onclick = function () {
 
     images[i].style.display = 'block';
 
+    slideLines[j].style.borderBottom = '2px solid white';
+
+    j++;
+
+    if ( j >=slideLines.length / 2) {
+
+        j = 0;
+    }
+
+    slideLines[j].style.borderBottom = '2px solid #f4bd50';
+
 }
+
+
